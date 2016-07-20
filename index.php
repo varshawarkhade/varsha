a339b5dbd2e3f2a76ab81bd34d892688dbfd4571
varshawarkhade <varsha.warkhade93@gmail.com>
2016-07-20 17:06:27 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Parth Ravani-Index</title>
+		<meta charset="utf-8"/>
+		<link  rel="stylesheet" href="style.css" type="text/css"/>
+		<meta name="viewport" content="width=device-width, initial-state=1.0">
+	</head>
+	<body class="body">
+		<header class="mainHeader">
+			<img src="clr.jpg">
+			<nav>
+				<ul>
+					<li> <a href="index.html">Home </a></li>
+					<li> <a href="portfolio.html">Portfolio</a></li>
+					<li> <a href="contact.html">Contact</a></li>
+				</ul>
+				<!--<button ></button>-->
+			</nav>
+		</header>
+		<div class="maincontent">
+			<div class="content">
+				<article class="topcontent">
+					<header>
+						<h2><p href="#" title="First post"> First Post </p></h2>
+					</header>	
+					<footer>
+						<p class="post-info">This post is related to parth</p>
+					</footer>
+					<content>
+						<p>This is Dummy Text. Responsive Web Design makes your web page look good on all 
+						devices (desktops, tablets, and phones).
+						Responsive Web Design is about using CSS and HTML to resize, hide, 
+						shrink, enlarge, or move the content to make it look good on any screen:</p>
+						<p>This is Dummy Text. Responsive Web Design makes your web page look good on all 
+						devices (desktops, tablets, and phones).
+						Responsive Web Design is about using CSS and HTML to resize, hide, 
+						shrink, enlarge, or move the content to make it look good on any screen:</p>
+					</content>
+				</article>
+				<article class="bottomcontent">
+					<header>
+						<h2><p href="#" title="Second post"> Second Post</p></h2>
+					</header>	
+					<footer>
+						<p class="post-info">This post is related to parth</p>
+					</footer>
+					<content>
+						<p>This is Dummy Text. Responsive Web Design makes your web page look good on all 
+						devices (desktops, tablets, and phones).
+						Responsive Web Design is about using CSS and HTML to resize, hide, 
+						shrink, enlarge, or move the content to make it look good on any screen:</p>
+						<p>This is Dummy Text. Responsive Web Design makes your web page look good on all 
+						devices (desktops, tablets, and phones).
+						Responsive Web Design is about using CSS and HTML to resize, hide, 
+						shrink, enlarge, or move the content to make it look good on any screen:</p>
+					</content>
+				</article>
+			</div>
+		</div>
+		
+		<aside class="top-sidebar">
+			<article>
+				<h2>Top Side Bar</h2>
+				<p>This is Dummy Text. Responsive Web Design is about using CSS and HTML to resize, hide,</p>
+			</article>
+		</aside>
+		<aside class="middle-sidebar">
+			<article>
+				<h2>Middle Side Bar</h2>
+				<p>This is Dummy Text. Responsive Web Design is about using CSS and HTML to resize, hide,</p>
+			</article>
+		</aside>
+		<aside class="bottom-sidebar">
+			<article>
+				<h2>Bottom Side Bar</h2>
+				<p>This is Dummy Text. Responsive Web Design is about using CSS and HTML to resize, hide,</p>
+			</article>
+		</aside>
+	<footer class="mainFooter">
+
+	<section>
+		<p> Copyright &copy; 2016 <a href="#"><b>Parth Ravani<b></a> &nbsp <a href="https://in.linkedin.com/in/parthravani"><img height="20px" width="20px" src="in.png" />
+				<a href="https://www.facebook.com/poetparth"><img height="20px" width="20px" src="fb.png" /></p>
+	<section>
+	</footer>
+	</body>
+<html>
